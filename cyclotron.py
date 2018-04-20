499d92bc16b4f7e32207252c1fc2160aeb244d09
Tony Alarcon <Tony@Tonys-MacBook-Pro-4.local>
2018-04-20 14:29:44 -0700

    Documentation Resources

--- cyclotron.py
+++ cyclotron.py
@@ -1,29 +1,40 @@
+###################################################################################
+#	Cyclotron Accelerator:
+#		Particle is subjected to constant magnetic field within the dee's that serves
+#		to keep particle in semi-circular motion. Electric field is applied within
+#		gaps which accelerates particle, increasing its energy.
+#	Lorentz Force Equation:
+#		Motion is described by F = q [ E + v x B]
+###################################################################################
+
+
 import numpy as np
 import math as math 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from decimal import Decimal
 
-c = 3.0E08
-e = [5000000.0, 0.0, 0.0] 
-b = [0.0, 0.0, -1.5]
-bmag = np.linalg.norm(b)
+
+speed_of_light = 3.0E08
+E_field = [5000000.0, 0.0, 0.0] 
+B_field = [0.0, 0.0, -1.5]
+bmag = np.linalg.norm(B_field)
 
 class Particle: 
-	def __init__(self, pos, vel, mass, charge): 
-		self.pos = pos
-		self.vel = vel
+	def __init__(self, position, velocity, mass, charge): 
+		self.position = position
+		self.velocity = velocity
 		self.mass = mass
 		self.charge = charge
 	
-	def radius(self,b):
+	def radius(self,b): #input magnetic field as argument in array
 		b = np.linalg.norm(b)
-		speed = np.linalg.norm(self.vel)
+		speed = np.linalg.norm(self.velocity)
 		radius = speed * self.mass / (self.charge * b)
 		return radius
 		
 	def period(self):
-		field = np.linalg.norm(b)
+		field = np.linalg.norm(B_field)
 		period = 2.0 * math.pi * self.mass /( field * self.charge )
 		return period
 		
@@ -31,9 +42,10 @@ class Particle:
 
 
 # choose a proton as the particle, describe in 3 Dimension 
-proton = Particle([0.00, 0.0, 0.0], [0.05*c, 0.0, 0.0], 1.67E-27, +1.60E-19)
+proton = Particle([0.00, 0.0, 0.0], [0.05*speed_of_light, 0.0, 0.0], 1.67E-27, +1.60E-19)
 
-gap_size = .5 * proton.radius(b)
+# size of gap between deed
+gap_size = .5 * proton.radius(B_field)
 
 
 ###################################################################################
@@ -44,26 +56,26 @@ gap_size = .5 * proton.radius(b)
 
 jumps =0
 jumps_max = 46
-count = 0
+i = 0
 
-def a( q_over_m, r, v ):
-	global count, jumps                # if you use the global statement, the variable will become available 
+def acceleration( charge_to_mass_ratio, r, v ):
+	global i, jumps                # if you use the global statement, the variable will become available 
 	if jumps >= jumps_max:			# "outside" the scope of the function, effectively becoming a global variable
 		# No acceleration
 		a = 0.0
 	elif r[0] >= 0 or r[0] <= -gap_size:
-		a = np.cross(v, b) 
-		a = a * q_over_m
-		if count:
+		a = np.cross(v, B_field) 
+		a = a * charge_to_mass_ratio
+		if i:  #Executes every time count is nonzero, occurs after particle enters dee from gap
 			jumps += 1
-			velo[jumps] = np.linalg.norm(v)
-			count = 0
+			velo[jumps] = np.linalg.norm(v) #saves new particle speed after gap cross
+			i = 0
 	else: 	
-		a = np.array(e)	
-		a = a * q_over_m
+		a = np.array(E_field)	
+		a = a * charge_to_mass_ratio
 		if r[1] > 0:
 			a = -a
-		count += 1
+		i += 1
 	return a
 
 
@@ -78,57 +90,54 @@ def a( q_over_m, r, v ):
 # initializes array that will hold the velocity of particle 
 # each time it gains velocity (crosses gap)
 velo = np.zeros((jumps_max + 1,))
-velo[0] = np.linalg.norm(proton.vel)
+velo[0] = np.linalg.norm(proton.velocity)
 
 
 def rk4(particle, iterations, desired_value):
-
+	#initialize arrays to be appended by rk4 results
 	RK4_pos = []
 	RK4_vel = []
 	
-	
-	
 	n = 400
 	h = particle.period() / n
-	q_over_m = particle.charge / particle.mass
+	charge_to_mass_ratio = particle.charge / particle.mass
 	
-	p_0 = np.array(particle.pos) 
-	v_0 = np.array(particle.vel) 
+	ini_pos = np.array(particle.position) 
+	ini_vel = np.array(particle.velocity) 
 	
 	for i in range(iterations + 10):
 		i += 1
-		p_i = p_0
-		v_i = v_0
+		p_i = ini_pos
+		v_i = ini_vel
 	
-		k1 = h * a( q_over_m, p_i, v_i )
+		k1 = h * acceleration( charge_to_mass_ratio, p_i, v_i )
 		l1 = h * v_i
 		
 		
-		k2 = h * a( q_over_m, p_0 + (l1 * 0.5), v_0 + (k1 * 0.5) )
-		l2 = h * (v_0 + (k1 * 0.5))
+		k2 = h * acceleration( charge_to_mass_ratio, ini_pos + (l1 * 0.5), ini_vel + (k1 * 0.5) )
+		l2 = h * (ini_vel + (k1 * 0.5))
 		
 
-		k3 = h * a( q_over_m, p_0 + (l2 * 0.5), v_0 + (k2 * 0.5) )
-		l3 = h * (v_0 + (k2 * 0.5))
+		k3 = h * acceleration( charge_to_mass_ratio, ini_pos + (l2 * 0.5), ini_vel + (k2 * 0.5) )
+		l3 = h * (ini_vel + (k2 * 0.5))
 	
-		k4 = h * a( q_over_m, p_0 + l3, v_0 + k3 )
-		l4 = h * (v_0 + k3)
+		k4 = h * acceleration( charge_to_mass_ratio, ini_pos + l3, ini_vel + k3 )
+		l4 = h * (ini_vel + k3)
 		
 
-		v_0 = v_0 + (k1 + 2.0 * (k2 + k3) + k4) / 6.0
-		p_0 = p_0 + (l1 + 2.0 * (l2 + l3) + l4) / 6.0
+		ini_vel = ini_vel + (k1 + 2.0 * (k2 + k3) + k4) / 6.0
+		ini_pos = ini_pos + (l1 + 2.0 * (l2 + l3) + l4) / 6.0
 	
-		
-		
-		if desired_value == 'velocity': 
-			vel_mag = np.linalg.norm(v_0)
-			RK4_vel.append(vel_mag)
-			value = RK4_vel
-		elif desired_value == 'position': 
-			RK4_pos.append(p_0)
-			value = RK4_pos
+		vel_mag = np.linalg.norm(ini_vel)
+		RK4_vel.append(vel_mag)
+		RK4_pos.append(ini_pos)
+			
+	#Controls which array function returns, resets jump count
+	if desired_value == 'velocity': 
+		value = RK4_vel
+	elif desired_value == 'position': 
+		value = RK4_pos
 	
-		
 	return value
 	
 
@@ -173,12 +182,13 @@ def position_plot(particle):
 	y = []
 	z = []
 	
-	x.append(particle.pos[0])
-	y.append(particle.pos[1])
-	z.append(particle.pos[2])
+	x.append(particle.position[0])
+	y.append(particle.position[1])
+	z.append(particle.position[2])
 	
 	results = rk4(proton , 10000 ,'position')	
 	
+	#extracts x,y,z coordinates
 	for i in results:
 		x.append(i[0])
 		y.append(i[1])
@@ -230,14 +240,15 @@ def vel_rad_plot(particle):
 
 	for item in range(1, len(velo)+1):
 		radius[item-1] = velo[item-1] * particle.mass / (particle.charge * bmag)
+		
 	
 	coordlabel = ["Radius (m)","Velocity (m/s)","Z-axis (m)"]
 	plot(coordlabel, "Beam Velocity at Various Extraction Radius", '2d', radius,velo)
 	
 
 
-velocity_plot(proton)
-jumps = 0 
+velocity_plot(proton) 
+jumps = 0
 position_plot(proton)
 vel_rad_plot(proton)
 plt.show()
